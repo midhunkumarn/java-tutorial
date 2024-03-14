da6732c07d9161b036e114ab038930526e023fa6
MIDHUN KUMAR N <130952023+midhunkumarn@users.noreply.github.com>
2024-03-14 10:55:21 +0530

    Add files via upload

---
+++ Array/array_concept.java
@@ -0,0 +1,15 @@
+public class array_concept {
+    public static void main(String a[]){
+        int nums[] = new int[4];
+        nums[0]=10;
+        nums[1]=20;
+        nums[2]=30;
+        nums[3]=40;
+
+        for(int i=0;i<4;i++){
+            System.out.println(nums[i]);
+        }
+
+    }
+}
+
---
+++ Array/multi_dimensional_array.java
@@ -0,0 +1,11 @@
+public class multi_dimensional_array {
+    public static void main(String args[]){
+        int nums[][] = new int[3][4];
+        for(int i = 0;i<3;i++){
+            for(int j=0;j<4;j++){
+                nums[i][j]=(int) (Math.random()*100);
+                System.out.print(nums[i][j]+" ");
+            }System.out.println();
+        }
+    }
+}
